cc75314cfedea7a702b2033a78ccfa1dc318a9b5
quocbao369 <quocbao27012002@gmail.com>
2023-10-09 07:16:24 +0700

    Add project files.

---
+++ HotelBookingAPI/Models/HotelBooking.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBookingAPI.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public int RooNumber { get; set; }
+        public string? ClientName { get; set; }
+    }
+}
